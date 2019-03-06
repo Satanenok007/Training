5ec5d003fb0854850f4286249417514753f5e9db
Satanenok007 <satanenok007@outlook.com>
2019-03-06 21:22:28 +0300

    Chapter_1.1

--- JavaScript_Algorithms/Chapter_1/js1.js
+++ JavaScript_Algorithms/Chapter_1/js1.js
@@ -1,1 +1,25 @@
-console.log("Hello, I am Nastya!");
+var arr = [2, 4, 6, 8, 10];
+function binary_search(arr, number) {
+    var foundValue;
+    var firstCell = 0;
+    var lastCell = arr.length - 1;
+    while (firstCell <= lastCell) {
+        var midCell = Math.floor((firstCell + lastCell) / 2);
+        var guessValue = arr[midCell];
+        if (guessValue === number) {
+            foundValue = guessValue;
+        }
+        if (guessValue > number) {
+            lastCell = midCell - 1;
+        } else {
+            firstCell = midCell + 1;
+        }
+    }
+    return foundValue;
+}
+
+var rez1 = binary_search(arr, 2); // 2
+var rezult = binary_search(arr, 6); // 6
+console.log(rez1);
+console.log(rezult);
+console.log(binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 9)); // 9
