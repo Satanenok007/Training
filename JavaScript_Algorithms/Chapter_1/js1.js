abd99956064d0231dec0090cac1b100af9e25e05
Nastya <satanenok007@outlook.com>
2020-12-05 21:28:04 +0300

    test1

--- JavaScript_Algorithms/Chapter_1/js1.js
+++ JavaScript_Algorithms/Chapter_1/js1.js
@@ -18,8 +18,8 @@ function binary_search(arr, number) {
     return foundValue;
 }
 
-var rez1 = binary_search(arr, 2); // 2
-var rezult = binary_search(arr, 6); // 6
+var rez1 = binary_search(arr, 2);
+var rezult = binary_search(arr, 6);
 console.log(rez1);
 console.log(rezult);
 console.log(binary_search([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11], 9)); // 9
