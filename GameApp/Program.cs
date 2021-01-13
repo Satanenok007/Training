ddf143ff75b17e8199481aa38a8b232cf0bbb6d4
Nastya <satanenok007@outlook.com>
2021-01-13 22:04:56 +0300

    working with multidimensional array

--- GameApp/Program.cs
+++ GameApp/Program.cs
@@ -18,43 +18,75 @@
 
 // Console.WriteLine("GameApp.");
 
-const int width = 10;
-const int heigth = 5;
-int y = 0;
-char[] m1 = new char[width * heigth];
+// const int width = 10;
+// const int heigth = 5;
+// int y = 0;
+// char[] m1 = new char[width * heigth];
 
-for (int i = 0; i < m1.Length; i++)
+// for (int i = 0; i < m1.Length; i++)
+// {
+//     if (i < width)
+//     {
+//         m1[i] = '1';
+//     }
+//     else if (i >= m1.Length - width)
+//     {
+//         m1[i] = '1';
+//     }
+//     else if (i % width == 0)
+//     {
+//         m1[i] = '1';
+//     }
+//     else if ((i + 1) % width == 0)
+//     {
+//         m1[i] = '1';
+//     }
+//     else
+//     {
+//         m1[i] = '0';
+//     }
+// }
+
+// for (int x = 0; x < m1.Length; x++)
+// {
+//     Console.Write(m1[x]);
+//     y++;
+
+//     if (y == width)
+//     {
+//         Console.WriteLine();
+//         y = 0;
+//     }
+// }
+
+int xLength = 10;
+int yLength = 5;
+char[,] m2 = new char[xLength, yLength];
+
+for (int y = 0; y < yLength; y++)
 {
-    if (i < width)
+    for (int x = 0; x < xLength; x++)
     {
-        m1[i] = '1';
-    }
-    else if (i >= m1.Length - width)
-    {
-        m1[i] = '1';
-    }
-    else if (i % width == 0) 
-    {
-        m1[i] = '1';
-    }
-    else if ((i + 1) % width == 0)
-    {
-        m1[i] = '1';
-    }
-    else
-    {
-        m1[i] = '0';
+        if (y == 0 || y == yLength - 1)
+        {
+            m2[x, y] = '1';
+        }
+        else if (x == 0 || x == xLength - 1)
+        {
+            m2[x, y] = '1';
+        }
+        else
+        {
+            m2[x,y] = '0';
+        }
     }
 }
 
-for (int x = 0; x < m1.Length; x++)
+for (int y = 0; y < yLength; y++)
 {
-    Console.Write(m1[x]);
-    y++;
-
-    if (y == width)
+    for (int x = 0; x < xLength; x++)
     {
-        Console.WriteLine();
-        y = 0;
+        Console.Write(m2[x, y]);
     }
+    Console.WriteLine();
 }
