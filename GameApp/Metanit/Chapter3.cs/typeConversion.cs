c2b3d193b899e97068a80ab5b7b4c614de556c87
Nastya <satanenok007@outlook.com>
2021-08-19 18:07:26 +0300

    repetition

--- GameApp/Metanit/Chapter3.cs/typeConversion.cs
+++ GameApp/Metanit/Chapter3.cs/typeConversion.cs
@@ -51,3 +51,50 @@ using System;
 //         }
 //     }
 // }
+
+
+namespace TypeConversion
+{
+    class Program
+    {
+        class Person
+        {
+            public string Name { get; set; }
+            public Person(string name)
+            {
+                Name = name;
+            }
+            public void Display()
+            {
+                Console.WriteLine($"Person {Name}");
+            }
+        }
+
+        class Employee : Person
+        {
+            public string Company { get; set; }
+            public Employee(string name, string company) : base(name)
+            {
+                Company = company;
+            }
+        }
+
+        class Client : Person
+        {
+            public string Bank { get; set; }
+            public Client(string name, string bank) : base(name)
+            {
+                Bank = bank;
+            }
+        }
+
+
+        static void Main(string[] args)
+        {
+            Employee employee = new Employee("Tom", "Microsoft");
+            Person person = employee;
+
+            Console.WriteLine(person.Name);
+        }
+    }
+}
