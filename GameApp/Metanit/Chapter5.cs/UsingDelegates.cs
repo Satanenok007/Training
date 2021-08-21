c89c4aa77e5326210c272155ce99604e4cecf6ad
Nastya <satanenok007@outlook.com>
2021-08-21 13:55:48 +0300

    repetition

--- GameApp/Metanit/Chapter5.cs/UsingDelegates.cs
+++ GameApp/Metanit/Chapter5.cs/UsingDelegates.cs
@@ -75,5 +75,45 @@ namespace UsingDelegates
                 Console.WriteLine(message);
             }
         }
+
+        class Example2
+        {
+            delegate int IntOperation(int i, int j);
+
+            class Program
+            {
+                // Организуем ряд методов
+                static int Sum(int x, int y)
+                {
+                    return x + y;
+                }
+
+                static int Prz(int x, int y)
+                {
+                    return x * y;
+                }
+
+                static int Del(int x, int y)
+                {
+                    return x / y;
+                }
+
+                static void Main()
+                {
+                    // Сконструируем делегат
+                    IntOperation op1 = new IntOperation(Sum);
+
+                    int result = op1(5, 10);
+                    Console.WriteLine("Сумма: " + result);
+
+                    // Изменим ссылку на метод
+                    op1 = new IntOperation(Prz);
+                    result = op1(5, 10);
+                    Console.WriteLine("Произведение: " + result);
+
+                    Console.ReadLine();
+                }
+            }
+        }
     }
 }
