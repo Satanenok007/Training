0eed9ca8ca14c7c06eceb9a51e21a925b7b8828f
Nastya <satanenok007@outlook.com>
2021-09-08 19:19:40 +0300

    Using delegates

--- GameApp/Metanit/Chapter5.cs/UsingDelegates.cs
+++ GameApp/Metanit/Chapter5.cs/UsingDelegates.cs
@@ -2,118 +2,87 @@ using System;
 
 namespace UsingDelegates
 {
-    class Program
+    class Account
     {
-        class Account
+        int _sum;
+
+        public delegate void AccountStateHandler(string message);
+        AccountStateHandler _del;
+
+        public void RegisterHandler(AccountStateHandler del)
         {
-            public delegate void AccountStateHandler(string message);
-            AccountStateHandler _del;
-            public void RegisterHandler(AccountStateHandler del)
-            {
-                _del = del;
-            }
-
-            int _sum;
-
-            public Account(int sum)
-            {
-                _sum = sum;
-            }
-
-            public int CurrentSum
-            {
-                get { return _sum; }
-            }
-
-            public void Put(int sum)
-            {
-                _sum += sum;
-            }
-
-            public void Withdraw(int sum)
-            {
-                if (sum <= _sum)
-                {
-                    _sum -= sum;
-                }
-            }
-
-            public void Withdraws(int sum)
-            {
-                if (sum <= _sum)
-                {
-                    _sum -= sum;
-
-                    if (_del != null)
-                    {
-                        _del($"Сумма {sum} снята со счета");
-                    }
-                }
-                else
-                {
-                    if (_del != null)
-                    {
-                        _del("Недостаточно денег на счете");
-                    }
-                }
-            }
-
-            static void Main(string[] args)
-            {
-                // создаем банковский счет
-                Account account = new Account(200);
-                // Добавляем в делегат ссылку на метод Show_Message
-                // а сам делегат передается в качестве параметра метода RegisterHandler
-                account.RegisterHandler(new Account.AccountStateHandler(Show_Message));
-                // Два раза подряд пытаемся снять деньги
-                account.Withdraws(100);
-                account.Withdraws(150);
-                Console.ReadLine();
-            }
-            private static void Show_Message(String message)
-            {
-                Console.WriteLine(message);
-            }
+            _del += del;
+        }
+        public void UnregisterHandler(AccountStateHandler del)
+        {
+            _del -= del;
         }
 
-        class Example2
+        public Account(int sum)
         {
-            delegate int IntOperation(int i, int j);
+            _sum = sum;
+        }
 
-            class Program
+        public int CurrentSum
+        {
+            get { return _sum; }
+        }
+
+        public void Put(int sum)
+        {
+            _sum += sum;
+        }
+
+        public void Withdraw(int sum)
+        {
+            if (sum <= _sum)
             {
-                // Организуем ряд методов
-                static int Sum(int x, int y)
+                _sum -= sum;
+
+                if (_del != null)
                 {
-                    return x + y;
-                }
-
-                static int Prz(int x, int y)
-                {
-                    return x * y;
-                }
-
-                static int Del(int x, int y)
-                {
-                    return x / y;
-                }
-
-                static void Main()
-                {
-                    // Сконструируем делегат
-                    IntOperation op1 = new IntOperation(Sum);
-
-                    int result = op1(5, 10);
-                    Console.WriteLine("Сумма: " + result);
-
-                    // Изменим ссылку на метод
-                    op1 = new IntOperation(Prz);
-                    result = op1(5, 10);
-                    Console.WriteLine("Произведение: " + result);
-
-                    Console.ReadLine();
+                    _del($"Сумма {sum} снята со счета");
                 }
             }
+            else
+            {
+                if (_del != null)
+                {
+                    _del("Недостаточно денег на счете");
+                }
+            }
+        }
+    }
+
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Account account = new Account(200);
+            Account.AccountStateHandler colorDelegate = new Account.AccountStateHandler(Color_Message);
+
+
+            account.RegisterHandler(new Account.AccountStateHandler(Show_Message));
+            account.RegisterHandler(colorDelegate);
+
+            account.Withdraw(100);
+            account.Withdraw(150);
+
+            account.UnregisterHandler(colorDelegate);
+            account.Withdraw(50);
+
+            Console.ReadLine();
+        }
+        private static void Show_Message(String message)
+        {
+            Console.WriteLine(message);
+        }
+        private static void Color_Message(string message)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(message);
+            Console.ResetColor();
         }
     }
 }
