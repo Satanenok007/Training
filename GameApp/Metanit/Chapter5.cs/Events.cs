58038bb0a506581fc45dabdc491dcc6530c5bd91
Nastya <satanenok007@outlook.com>
2021-09-15 18:56:31 +0300

    Events

--- GameApp/Metanit/Chapter5.cs/Events.cs
+++ GameApp/Metanit/Chapter5.cs/Events.cs
@@ -116,4 +116,50 @@ class ManagingHandlers
         }
         private static void DisplayMessage(string message) => Console.WriteLine(message);
     }
+
+    class Event
+    {
+        delegate void UI();
+        class MyEvent
+        {
+            public event UI UserEvent;
+            public void OnUserEvent()
+            {
+                UserEvent();
+            }
+        }
+
+        class UserInfo
+        {
+            string uiName, uiFamily;
+            int uiAge;
+            public UserInfo(string Name, string Family, int Age)
+            {
+                this.uiName = Name;
+                this.uiFamily = Family;
+                this.uiAge = Age;
+            }
+
+            public string Name { set { uiName = value; } get { return uiName; } }
+            public string Family { set { uiFamily = value; } get { return uiFamily; } }
+            public int Age { set { uiAge = value; } get { return uiAge; } }
+
+            public void UserInfoHandler()
+            {
+                Console.WriteLine("Событие вызвано!\n");
+                Console.WriteLine("Имя: {0}\nФамилия: {1}\nВозраст: {2}", Name, Family, Age);
+            }
+
+            class Program
+            {
+                static void Main()
+                {
+                    MyEvent evt = new MyEvent();
+                    UserInfo user1 = new UserInfo(Name: "Sofia", Family: "Kmita", Age: 16);
+                    evt.UserEvent += user1.UserInfoHandler;
+                    evt.OnUserEvent();
+                }
+            }
+        }
+    }
 }
