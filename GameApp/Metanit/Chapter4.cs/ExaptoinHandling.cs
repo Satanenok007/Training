36073cfddf4d399ae57e0bc13f24f7ccf39821f9
Nastya <satanenok007@outlook.com>
2021-09-17 19:05:43 +0300

    Exaption hadling

--- GameApp/Metanit/Chapter4.cs/ExaptoinHandling.cs
+++ GameApp/Metanit/Chapter4.cs/ExaptoinHandling.cs
@@ -23,4 +23,23 @@ namespace ExaptionHadling
             Console.WriteLine("Конец программы");
         }
     }
+
+    class Example2
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int x;
+            string input = Console.ReadLine();
+            if (Int32.TryParse(input, out x))
+            {
+                x *= x;
+                Console.WriteLine("Квадрат числа: " + x);
+            }
+            else
+            {
+                Console.WriteLine("Некорректный ввод");
+            }
+        }
+    }
 }
