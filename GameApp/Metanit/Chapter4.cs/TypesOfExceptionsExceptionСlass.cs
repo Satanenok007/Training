28134503b55bfae0487f987fad03f9583d6b5df8
Nastya <satanenok007@outlook.com>
2021-09-18 17:10:08 +0300

    Type exeption

--- GameApp/Metanit/Chapter4.cs/TypesOfExceptionsExceptionСlass.cs
+++ GameApp/Metanit/Chapter4.cs/TypesOfExceptionsExceptionСlass.cs
@@ -47,4 +47,44 @@ namespace TypesOfExceptionsExceptionСlass
             }
         }
     }
+
+    class Exaple3
+    {
+        [Serializable]
+        public class MyException : ApplicationException
+        {
+            public MyException() { }
+            public MyException(string message) : base(message) { }
+            public MyException(string message, Exception ex) : base(message) { }
+            protected MyException(System.Runtime.Serialization.SerializationInfo info,
+                System.Runtime.Serialization.StreamingContext contex)
+                : base(info, contex) { }
+        }
+
+        class Program
+        {
+            static int MyDel(int x, int y)
+            {
+                if (y == 0)
+                {
+                    MyException exc = new MyException("ОШИБКА: деление на 0");
+                    exc.HelpLink = "http:\\professorweb.ru";
+                    throw exc;
+                }
+                return x / y;
+            }
+
+            static void Main()
+            {
+                try
+                {
+                    MyDel(5, 0);
+                }
+                catch (MyException)
+                {
+                    Console.WriteLine("Возникла ошибка");
+                }
+            }
+        }
+    }
 }
