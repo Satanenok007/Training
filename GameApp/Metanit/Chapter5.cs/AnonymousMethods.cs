2834457c586e249c5f75189fcf5996dc8cd6f8df
Nastya <satanenok007@outlook.com>
2021-09-25 22:43:24 +0300

    Anonymous methods

--- GameApp/Metanit/Chapter5.cs/AnonymousMethods.cs
+++ GameApp/Metanit/Chapter5.cs/AnonymousMethods.cs
@@ -68,4 +68,74 @@ namespace AnonymousMethods
             Console.WriteLine(d);
         }
     }
+
+    class Ex1
+    {
+        class Option1
+        {
+            class Program
+            {
+                delegate void MessageHandler(string message);
+                static void Main(string[] args)
+                {
+                    MessageHandler handler = delegate (string mes)
+                    {
+                        Console.WriteLine(mes);
+                    };
+                    handler("Option1");
+
+                }
+            }
+        }
+
+        class Option2
+        {
+            class Program
+            {
+                delegate void MessageHandler(string message);
+                static void Main(string[] args)
+                {
+                    ShowMessage("Option2", delegate (string mes)
+                    {
+                        Console.WriteLine(mes);
+                    });
+
+                }
+                static void ShowMessage(string mes, MessageHandler handler)
+                {
+                    handler(mes);
+                }
+            }
+        }
+
+        class Option3
+        {
+            class Program
+            {
+                delegate void MessageHandler(string message);
+                static void Main(string[] args)
+                {
+                    MessageHandler handler = delegate
+                    {
+                        Console.WriteLine("анонимный метод");
+                    };
+                    handler("Option3");
+                }
+            }
+        }
+
+        class Option4
+        {
+            delegate int Operation(int x, int y);
+            static void Main(string[] args)
+            {
+                Operation operation = delegate (int x, int y)
+                {
+                    return x + y;
+                };
+                int sum = operation(16, 78);
+                Console.WriteLine(sum);
+            }
+        }
+    }
 }
