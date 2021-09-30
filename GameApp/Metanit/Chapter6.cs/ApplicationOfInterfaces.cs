9fbe16ac4b9d83f7b8b494faa9e5b5e2216fb5ef
Nastya <satanenok007@outlook.com>
2021-09-30 16:32:02 +0300

    Application of interfaces

---
+++ GameApp/Metanit/Chapter6.cs/ApplicationOfInterfaces.cs
@@ -0,0 +1,119 @@
+using System;
+namespace ApplicationOfInterfaces
+{
+    class Program
+    {
+        interface IMovable
+        {
+            void Move();
+        }
+
+        class Person : IMovable
+        {
+            public void Move()
+            {
+                Console.WriteLine("Человек идет");
+            }
+        }
+        struct Car : IMovable
+        {
+            public void Move()
+            {
+                Console.WriteLine("Машина едет");
+            }
+
+        }
+
+        class Programs
+        {
+            static void Action(IMovable movable)
+            {
+                movable.Move();
+            }
+            static void Main(string[] args)
+            {
+                Person person = new Person();
+                Car car = new Car();
+                Action(person);
+                Action(car);
+            }
+        }
+
+        class ImplementationOfDefaultInterfaces
+        {
+            class Program
+            {
+                static void Main(string[] args)
+                {
+                    IMovable p1 = new Person();
+                    Car tesla = new Car();
+                    p1.Move();
+                    tesla.Move();
+                }
+            }
+
+            interface IMovable
+            {
+                void Move()
+                {
+                    Console.WriteLine("Walking");
+                }
+            }
+            class Person : IMovable { }
+            class Car : IMovable
+            {
+                public void Move()
+                {
+                    Console.WriteLine("Driving");
+                }
+            }
+        }
+
+        class MultipleImplementationOfInterfaces
+        {
+            interface IAccount
+            {
+                int CurrentSum { get; }
+                void Put(int sum);
+                void Withdraw(int sum);
+            }
+            interface IClient
+            {
+                string Name { get; set; }
+            }
+            class Client : IAccount, IClient
+            {
+                int _sum;
+                public string Name { get; set; }
+                public Client(string name, int sum)
+                {
+                    Name = name;
+                    _sum = sum;
+                }
+
+                public int CurrentSum { get { return _sum; } }
+
+                public void Put(int sum) { _sum += sum; }
+
+                public void Withdraw(int sum)
+                {
+                    if (_sum >= sum)
+                    {
+                        _sum -= sum;
+                    }
+                }
+            }
+            class Program
+            {
+                static void Main(string[] args)
+                {
+                    Client client = new Client("Tom", 200);
+                    client.Put(30);
+                    Console.WriteLine(client.CurrentSum);
+                    client.Withdraw(100);
+                    Console.WriteLine(client.CurrentSum);
+                }
+            }
+        }
+    }
+}
