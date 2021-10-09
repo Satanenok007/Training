e157a3192be3a0652d0b68be37c5e0d041eade5a
Nastya <satanenok007@outlook.com>
2021-10-09 17:10:16 +0300

    Reenactors, Switch patterns

---
+++ GameApp/Metanit/Chapter7.cs/Reenactors.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace Reenactors
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+        public void Deconstruct(out string name, out int age)
+        {
+            name = this.Name;
+            age = this.Age;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person { Name = "Mike", Age = 23 };
+
+            (string name, int age) = p1;
+
+            Console.WriteLine(name);
+            Console.WriteLine(age);
+        }
+    }
+}
