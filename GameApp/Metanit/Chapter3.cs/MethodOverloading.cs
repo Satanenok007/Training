157dc67f235886dcf4f0d7f92f22f962b545f307
Nastya <satanenok007@outlook.com>
2021-11-21 18:27:00 +0300

    Method overloading

--- GameApp/Metanit/Chapter3.cs/MethodOverloading.cs
+++ GameApp/Metanit/Chapter3.cs/MethodOverloading.cs
@@ -22,14 +22,40 @@ namespace MethodOverloading
             Console.WriteLine(result);
         }
 
-        class Program
+        class C2
         {
-            static void F2(string[] args)
+            public void F2(int n1, int n2)
             {
-                C1 obj1 = new C1();
-                obj1.F1(1,2);
-                obj1.F1(1,2,3);
-                obj1.F1(1,2,3,4);
+                int res = n1 + n2;
+                Console.WriteLine(res);
+            }
+
+            public void F2(int n1, int n2, int n3)
+            {
+                int res = n1 + n2 + n3;
+                Console.WriteLine(res);
+            }
+
+            public void F2(int n1, int n2, int n3, int n4)
+            {
+                int res = n1 + n2 + n3 + n4;
+                Console.WriteLine(res);
+            }
+
+            class Program
+            {
+                static void F2(string[] args)
+                {
+                    C1 obj1 = new C1();
+                    obj1.F1(1, 2);
+                    obj1.F1(1, 2, 3);
+                    obj1.F1(1, 2, 3, 4);
+
+                    C2 obj2 = new C2();
+                    obj2.F2(10, 11);
+                    obj2.F2(4, 9, 3);
+                    obj2.F2(5, 7, 12, 8);
+                }
             }
         }
     }
