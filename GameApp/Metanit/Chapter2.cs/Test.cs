8ce0c9e2bf580adf81fae9b72e7e3d46e52565c1
Nastya <satanenok007@outlook.com>
2021-11-25 19:12:51 +0300

    tasks with arrays

--- GameApp/Metanit/Chapter2.cs/Test.cs
+++ GameApp/Metanit/Chapter2.cs/Test.cs
@@ -4,7 +4,7 @@ namespace Test
 {
     class C1
     {
-        static void F1()
+        static void Main()
         {
             int[,] arr1 = new int[,] { { 1, 2, 3 }, { 4, 5, 6 } };
             int rows = arr1.GetUpperBound(0) + 1;
@@ -13,6 +13,13 @@ namespace Test
             char[,] arr2 = new char[,] { { '1', '1', '1' }, { '2', '2', '2' } };
             int rows2 = arr2.GetUpperBound(0) + 1;
             int columns2 = arr2.Length / rows2;
+
+            int[] m1 = new int[] { 1, -2, 3, -1, 2, -3, 4 };
+            int count = 0;
+            int temp;
+
+            int[] m2 = new int[] { 1, 2, 3, 4, 5 };
+            int[] m3 = new int[5];
 
             for (int y = 0; y < rows; y++)
             {
@@ -36,6 +43,32 @@ namespace Test
                 Console.WriteLine();
                 Console.Write("rows");
                 Console.WriteLine();
+            }
+
+            for (int x = 0; x < m1.Length; x++)
+            {
+                if (m1[x] > 0)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine(count);
+
+            for (int x = 0; x < m1.Length - 1; x++)
+            {
+                for (int y = x + 1; y < m1.Length; y++)
+                {
+                    if (m1[x] > m1[y])
+                    {
+                        temp = m1[x];
+                        m1[x] = m1[y];
+                        m1[y] = temp;
+                    }
+                }
+            }
+            for (int x = 0; x < m1.Length; x++)
+            {
+                Console.WriteLine(m1[x]);
             }
         }
     }
