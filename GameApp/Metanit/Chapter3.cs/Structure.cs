212b7f7c7b9c98094b1e01b8631d62dec2b27863
Nastya <satanenok007@outlook.com>
2021-12-10 17:50:51 +0300

    Classes and objects, Constructors, initializers and constructors, The Program class and the Main method, Top-level programs, Structure

--- GameApp/Metanit/Chapter3.cs/Structure.cs
+++ GameApp/Metanit/Chapter3.cs/Structure.cs
@@ -12,7 +12,26 @@ namespace Structure
 
             C2 obj2 = new C2 { numb1 = 20, numb2 = 40 };
             obj2.GetInfo();
-            // C2 obj3 = obj2 with { numb1 = 33 };
+            
+
+            C3 obj3 = new C3() {n1 = 1, n2 = 2};
+            obj3.F1();
+
+            C3 obj4 = new C3();
+            obj4.n1 = 3;
+            obj4.n2 = 4;
+            obj4.F1();
+
+            C3 obj5 = new();
+            obj5.n1 = 5;
+            obj5.n2 = 6;
+            obj5.F1();
+
+            C3 obj6;
+            obj6.n1 = 7;
+            obj6.n2 = 8;
+            obj6.F1();
+
         }
     }
 
@@ -28,6 +47,22 @@ namespace Structure
         public void GetInfo()
         {
             Console.WriteLine($"numb1: {numb1}, numb2: {numb2}");
+        }
+    }
+
+    struct C3
+    {
+        public int n1;
+        public int n2;
+
+        public C3(int n1 = 0, int n2 = 0)
+        {
+            this.n1 = n1;
+            this.n2 = n2;
+        }
+        public void F1()
+        {
+            Console.WriteLine($"{n1}, {n2}");
         }
     }
 }
