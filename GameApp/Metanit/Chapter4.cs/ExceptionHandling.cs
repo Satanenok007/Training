6037f73c1360fd1b5d3b62c5ee5d4cb7cb4dcefd
Nastya <satanenok007@outlook.com>
2021-12-19 21:01:45 +0300

    Exception handling, removing unnecessary

---
+++ GameApp/Metanit/Chapter4.cs/ExceptionHandling.cs
@@ -0,0 +1,37 @@
+using System;
+
+namespace ExceptionHandling
+{
+    class C1
+    {
+        static void F1()
+        {
+            int n1;
+            string temp = Console.ReadLine();
+            if (Int32.TryParse(temp, out n1))
+            {
+                n1 += n1;
+                Console.WriteLine("sum: " + n1);
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+
+            try
+            {
+                int n2 = 5;
+                int n3 = n2 / 0;
+                Console.WriteLine($"try: {n2}");
+            }
+            catch
+            {
+                Console.WriteLine("catch");
+            }
+            finally
+            {
+                Console.WriteLine("finally");
+            }
+        }
+    }
+}
