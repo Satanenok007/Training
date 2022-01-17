16e8a4afbb65a4b9f59c9909e953d89732a8db16
Nastya <satanenok007@outlook.com>
2022-01-17 20:41:18 +0300

    Closures

---
+++ GameApp/Metanit/Chapter5.cs/Closures.cs
@@ -0,0 +1,44 @@
+using System;
+
+namespace Closures
+{
+
+    class C1
+    {
+        delegate int Operation(int n);
+        static void F1(string[] args)
+        {
+            var obj1 = F2();
+            obj1();
+            obj1();
+            obj1();
+
+            Action F2()
+            {
+                int a = 5;
+                void F3()
+                {
+                    a++;
+                    Console.WriteLine(a);
+                }
+                return F3;
+            }
+
+            var obj2 = F4(5);
+
+            Console.WriteLine(obj2(5));
+            Console.WriteLine(obj2(6));
+            Console.WriteLine(obj2(7));
+
+            Operation F4(int n1)
+            {
+                int F5(int n2)
+                {
+                    return n1 * n2;
+                }
+                return F5;
+            }
+
+        }
+    }
+}
